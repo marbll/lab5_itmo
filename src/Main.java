dca047c4ca29ee1bf096f66deaf756dcf9c01639
marbll <64865543+marbll@users.noreply.github.com>
2020-05-29 08:21:41 +0300

    Добавляем javadoc

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 package Labs;
 
 import java.io.IOException;
-
+/**
+ * @autors Margarita Blinova
+ * @version 2.0
+ * @year 2020
+ */
 public class Main {
-   public static void main(String[] args) throws IOException {
-      try {
-         String s = args[0];
-         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("Работа программы завершена!");
-         }));
-         Commander commander = new Commander(new CollectionManager(s));
-         commander.interactiveMod();
-      } catch (ArrayIndexOutOfBoundsException var3) {
-         System.out.println("Путь до файла xml нужно передать через аргумент командной строки.");
-         System.exit(1);
-      }
-
-   }
+    public static void main(String[] args) throws IOException{
+        try {
+            String s = args[0];
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.println("Работа программы завершена!")));
+            Commander commander = new Commander(new CollectionManager(s));
+            commander.interactiveMod();
+        } catch(ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Путь до файла xml нужно передать через аргумент командной строки.");
+            System.exit(1);
+        }
+    }
 }
