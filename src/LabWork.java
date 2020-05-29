eb1bf8bcc63c8b4e70a182df8b4b7bef2dc74c42
marbll <64865543+marbll@users.noreply.github.com>
2020-05-29 08:20:56 +0300

    Добавляем javadoc

--- src/LabWork.java
+++ src/LabWork.java
@@ -1,103 +1,110 @@
 package Labs;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
+/**
+ * Класс, объекты которого хранятся в коллекции
+ */
 public class LabWork implements Comparable<LabWork> {
-   private Integer id;
-   private String name;
-   private Coordinates coordinates;
-   private LocalDate creationDate;
-   private long minimalPoint;
-   private String description;
-   private Double averagePoint;
-   private Difficulty difficulty;
-   private Discipline discipline;
+    private Integer id; //Поле не может быть null, Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
+    private String name; //Поле не может быть null, Строка не может быть пустой
+    private Coordinates coordinates; //Поле не может быть null
+    private LocalDate creationDate; //Поле не может быть null, Значение этого поля должно генерироваться автоматически
+    private long minimalPoint; //Значение поля должно быть больше 0
+    private String description; //Строка не может быть пустой, Поле не может быть null
+    private Double averagePoint; //Поле не может быть null, Значение поля должно быть больше 0
+    private Difficulty difficulty; //Поле может быть null
+    private Discipline discipline; //Поле не может быть null
 
-   public LabWork(Integer id, String name, Coordinates coordinates, LocalDate creationDate, long minimalPoint, String description, Double averagePoint, Difficulty difficulty, Discipline discipline) {
-      this.id = id;
-      this.name = name;
-      this.coordinates = coordinates;
-      this.creationDate = creationDate;
-      this.minimalPoint = minimalPoint;
-      this.description = description;
-      this.averagePoint = averagePoint;
-      this.difficulty = difficulty;
-      this.discipline = discipline;
-   }
+    public LabWork(Integer id, String name, Coordinates coordinates, LocalDate creationDate, long minimalPoint, String description, Double averagePoint, Difficulty difficulty, Discipline discipline){
+        this.id = id;
+        this.name = name;
+        this.coordinates = coordinates;
+        this.creationDate = creationDate;
+        this.minimalPoint = minimalPoint;
+        this.description = description;
+        this.averagePoint = averagePoint;
+        this.difficulty = difficulty;
+        this.discipline = discipline;
+    }
 
-   public int compareTo(LabWork e) {
-      if (!this.averagePoint.equals(e.getAveragePoint())) {
-         return this.averagePoint.compareTo(e.getAveragePoint());
-      } else if (this.minimalPoint - e.getMinimalPoint() != 0L) {
-         return (int)(this.minimalPoint - e.getMinimalPoint());
-      } else if (this.difficulty != null && e.getDifficulty() == null) {
-         return 1;
-      } else if (this.difficulty == null && e.getDifficulty() != null) {
-         return -1;
-      } else if (this.difficulty != null && e.getDifficulty() != null && this.difficulty.compareTo(e.getDifficulty()) != 0) {
-         return this.difficulty.compareTo(e.getDifficulty());
-      } else if (this.discipline.compareTo(e.getDiscipline()) != 0) {
-         return this.discipline.compareTo(e.getDiscipline());
-      } else if (this.coordinates.compareTo(e.getCoordinates()) != 0) {
-         return this.coordinates.compareTo(e.getCoordinates());
-      } else {
-         return this.description.compareTo(e.getDescription()) != 0 ? this.description.compareTo(e.getDescription()) : this.name.compareTo(e.getName());
-      }
-   }
+    /**
+     * Сравнивает данную лабораторную работу с переданной в качестве параметра по averagePoint (другим полям в случае равенства)
+     * @param e Лабораторная работа, сравниваемая с данной
+     * @return число
+     */
+    @Override
+    public int compareTo(LabWork e) {
+        if (!averagePoint.equals(e.getAveragePoint())) return (int) (averagePoint.compareTo(e.getAveragePoint()));
+        if (minimalPoint - e.getMinimalPoint() != 0) return (int) (minimalPoint - e.getMinimalPoint());
+        if (difficulty != null && e.getDifficulty() == null) return 1;
+        else if (difficulty == null && e.getDifficulty() != null) return -1;
+        else if (difficulty != null && e.getDifficulty() != null && difficulty.compareTo(e.getDifficulty()) != 0) return difficulty.compareTo(e.getDifficulty());
+        if (discipline.compareTo(e.getDiscipline()) != 0) return discipline.compareTo(e.getDiscipline());
+        if (coordinates.compareTo(e.getCoordinates()) != 0) return coordinates.compareTo(e.getCoordinates());
+        if (description.compareTo(e.getDescription()) != 0) return description.compareTo(e.getDescription());
+        return name.compareTo(e.getName());
+    }
 
-   public String getName() {
-      return this.name;
-   }
+    public String getName() {
+        return name;
+    }
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+    public long getMinimalPoint() {
+        return minimalPoint;
+    }
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+    public Discipline getDiscipline() {
+        return discipline;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public Double getAveragePoint() {
+        return averagePoint;
+    }
 
-   public Coordinates getCoordinates() {
-      return this.coordinates;
-   }
+    @Override
+    public String toString() {
+        return "LabWork { " +
+                "id: " + id + ", name: " + name + "," + coordinates.toString() + ", creationDate:"
+                + creationDate + ", minimalPoint:" + minimalPoint + ", description:" + description + ", averagePoint:" + averagePoint +
+                ", difficulty:" + difficulty + ", " + discipline.toString() + "}" ;
+    }
 
-   public Integer getId() {
-      return this.id;
-   }
+    /**
+     * проверяет на равенство объекты LabWork (равенство только при равенстве всех полей)
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LabWork)) return false;
+        LabWork labWork = (LabWork) o;
+        return id.equals(labWork.getId()) &&
+                name.equals(labWork.getName()) &&
+                coordinates.equals(labWork.getCoordinates()) &&
+                creationDate.equals(labWork.getCreationDate()) &&
+                minimalPoint == labWork.getMinimalPoint() &&
+                description.equals(labWork.getDescription()) &&
+                averagePoint.equals(labWork.getAveragePoint()) &&
+                difficulty.equals(labWork.getDifficulty()) &&
+                discipline.equals(labWork.getDiscipline());
+    }
 
-   public LocalDate getCreationDate() {
-      return this.creationDate;
-   }
-
-   public long getMinimalPoint() {
-      return this.minimalPoint;
-   }
-
-   public Difficulty getDifficulty() {
-      return this.difficulty;
-   }
-
-   public Discipline getDiscipline() {
-      return this.discipline;
-   }
-
-   public String getDescription() {
-      return this.description;
-   }
-
-   public Double getAveragePoint() {
-      return this.averagePoint;
-   }
-
-   public String toString() {
-      return "LabWork { id: " + this.id + ", name: " + this.name + "," + this.coordinates.toString() + ", creationDate:" + this.creationDate + ", minimalPoint:" + this.minimalPoint + ", description:" + this.description + ", averagePoint:" + this.averagePoint + ", difficulty:" + this.difficulty + ", " + this.discipline.toString() + "}";
-   }
-
-   public boolean equals(Object o) {
-      if (this == o) {
-         return true;
-      } else if (!(o instanceof LabWork)) {
-         return false;
-      } else {
-         LabWork labWork = (LabWork)o;
-         return this.id.equals(labWork.getId()) && this.name.equals(labWork.getName()) && this.coordinates.equals(labWork.getCoordinates()) && this.creationDate.equals(labWork.getCreationDate()) && this.minimalPoint == labWork.getMinimalPoint() && this.description.equals(labWork.getDescription()) && this.averagePoint.equals(labWork.getAveragePoint()) && this.difficulty.equals(labWork.getDifficulty()) && this.discipline.equals(labWork.getDiscipline());
-      }
-   }
-
-   public int hashCode() {
-      return Objects.hash(new Object[]{this.id, this.name, this.coordinates, this.creationDate, this.minimalPoint, this.description, this.averagePoint, this.difficulty, this.discipline});
-   }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, coordinates, creationDate, minimalPoint, description, averagePoint, difficulty, discipline);
+    }
 }
